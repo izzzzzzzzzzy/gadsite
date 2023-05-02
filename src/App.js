dba9308bd8a59c86b88c4754ff06d54bbc0cc013
davkri22 <dkriz1662@gmail.com>
2023-05-02 16:45:52 -0400

    added a decent looking header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
+import Header from './Components/Header/Header';
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Navbar />
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        </BrowserRouter>
+        <div className="App">
+            <Header />
+            <body>
+                <p>
+                    Stuff goes here
+                </p>
+            </body>
+        </div>
     );
 }
 
